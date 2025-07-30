9095b29c42198741ee87f24bf6cfe831cbda6a8d
KryzlKate <castaneda.kryzlkate26@gmail.com>
2025-07-30 09:44:58 +0800

    Update Layout.jsx

--- src/assets/Layout/Layout.jsx
+++ src/assets/Layout/Layout.jsx
@@ -94,30 +94,6 @@ function Templates() {
     },
   ];
 
-    const layoutTemplates = [
-    {
-      id: "layout1",
-      src: "layout1.png",
-      title: "Basic Layout Design",
-      description: "Simple layout design service",
-      templateType: "layout",
-    },
-    {
-      id: "layout2",
-      src: "layout2.png",
-      title: "Advanced Layout Design",
-      description: "Complex layout design service",
-      templateType: "layout",
-    },
-    {
-      id: "layout3",
-      src: "layout3.png",
-      title: "Custom Layout Design",
-      description: "Fully customized layout design",
-      templateType: "layout",
-    },
-  ];
-
 const renderTemplates = (templates) =>
   templates.map((template, index) => (
     <div
@@ -150,12 +126,6 @@ const renderTemplates = (templates) =>
         <h2 className="template-heading">POWERPOINT PRESENTATIONS</h2>
         <div className="template-row">{renderTemplates(pptTemplates)}</div>
       </div>
-
-      <div className="template-section">
-        <h2 className="template-heading">LAYOUT DESIGNS</h2>
-        <div className="template-row">{renderTemplates(layoutTemplates)}</div>
-      </div>
-
 
       {selectedTemplate && (
         <div
